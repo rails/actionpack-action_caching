c3875d149f7cdbcc5eff543d86b268ff365381e8
Alex Yusupov <alex@klepa.co>
2015-06-12 10:06:47 -0400

    Fixed Control-Cache headers logic

--- lib/action_controller/caching/actions.rb
+++ lib/action_controller/caching/actions.rb
@@ -176,8 +176,8 @@ module ActionController
           body = controller.render_to_string(text: body, layout: true) unless cache_layout
 
           if @store_options[:expires_in]
-            expires_in = @store_options.delete(:expires_in)
-            controller.expires_in expires_in, @store_options
+            headers = @store_options.dup
+            controller.expires_in headers.delete(:expires_in), headers
           end
           controller.response_body = body
           controller.content_type = Mime[cache_path.extension || :html]
