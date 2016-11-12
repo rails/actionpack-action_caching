5438d91095507f58459575b6767efabfb8a01432
Andrew White <andrew.white@unboxed.co>
2016-11-12 10:46:47 +0000

    Add random test ordering where available

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -6,3 +6,6 @@ require 'action_controller/action_caching'
 
 FIXTURE_LOAD_PATH = File.expand_path('../fixtures', __FILE__)
 
+if ActiveSupport.respond_to?(:test_order)
+  ActiveSupport.test_order = :random
+end
