7be3cadd815c95b3327d9284dacba83fd84b74b0
Jeff Keen <jeff@keen.me>
2020-11-11 10:49:25 -0600

    Simplify action caching initialization, fix Rails 6.1 incompatibility

--- lib/action_controller/action_caching.rb
+++ lib/action_controller/action_caching.rb
@@ -2,12 +2,8 @@ require "action_controller/caching/actions"
 
 module ActionController
   module Caching
-    eager_autoload do
-      autoload :Actions
-    end
-
     include Actions
   end
 end
 
-ActionController::Base.send(:include, ActionController::Caching::Actions)
+ActionController::Base.include(ActionController::Caching::Actions)
